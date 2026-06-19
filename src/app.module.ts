5b3120e6bff348e8034930c21bcd8728d5f59810
SlavikusVOG <slavaolish@gmail.com>
2026-06-19 16:12:59 +0300

    feat: register AnonymousSessionService in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CommercetoolsService } from './commercetools/commercetools.service';
+import { AnonymousSessionService } from './commercetools/anonymous-session/anonymous-session.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService, CommercetoolsService],
+  providers: [AppService, CommercetoolsService, AnonymousSessionService],
 })
 export class AppModule {}
