62a2043d462791c8bef0b17eb90e5e8f606d415e
SlavikusVOG <slavaolish@gmail.com>
2026-06-24 20:35:41 +0300

    feat: enhance DTOs with Swagger documentation examples

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,12 +1,12 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export class LoginDto {
-  @ApiProperty({ description: 'user@example.com' })
+  @ApiProperty({ description: 'user@example.com', example: 'user@example.com' })
   email: string;
 
-  @ApiProperty({ description: 'password' })
+  @ApiProperty({ description: 'password', example: 'password' })
   password: string;
 
-  @ApiPropertyOptional({ description: 'store-key' })
+  @ApiPropertyOptional({ description: 'store-key', example: 'store-key' })
   storeKey?: string;
 }
